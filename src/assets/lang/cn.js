1f60a1e425410adda313d304d7a367c26fc7cf4d
chenpeng <2455915115@qq.com>
2020-06-27 15:45:37 +0800

    填充中英文转换

--- src/assets/lang/cn.js
+++ src/assets/lang/cn.js
@@ -1,3 +1,30 @@
-export const message = {
-    home: '首页'
-}
+export const headerTransInfo = {
+  home: '首页',
+  product: {
+    title: '产品中心',
+    jiaDe: '贾德系列',
+    molandi: '莫兰迪系列',
+    miss: '密斯系列',
+    dikesen: '迪克森系列',
+    bier: '比尔系列',
+    kelinte: '克林特系列',
+    botiqieli: '波提切利系列'
+  },
+  brandStory: '品牌故事',
+  explorHall: '探索展厅',
+  temingDynamic: '特铭动态',
+  joinUs: {
+    title: '加入我们',
+    BrandPresence: '品牌入驻',
+    recruit: '招贤纳士'
+  },
+  enterMall: '进入商城'
+};
+export const footerTransInfo = {
+  linkFirst: '友情链接1',
+  linkSecond: '友情链接2',
+  linkThird: '友情链接3',
+  linkForth: '友情链接4',
+  companyName: 'Copyright @2010 - 2020上海特铭家具有限公司',
+  companyNumber: '沪ICP备06025014号'
+};
--- src/assets/lang/en.js
+++ src/assets/lang/en.js
@@ -1,3 +1,30 @@
-export const message = {
-    home: 'home'
-}
+export const headerTransInfo = {
+  home: 'home',
+  product: {
+    title: 'product',
+    jiaDe: 'Judd series',
+    molandi: 'molandi series',
+    miss: 'miss series',
+    dikesen: 'dikesen series',
+    bier: 'bier series',
+    kelinte: 'kelinte series',
+    botiqieli: 'botiqieli series'
+  },
+  brandStory: 'Brand Story',
+  explorHall: 'Explor Hall',
+  temingDynamic: 'TeMing Dynamic',
+  joinUs: {
+    title: 'Join Us',
+    BrandPresence: 'Brand Presence',
+    recruit: 'recruit'
+  },
+  enterMall: 'Enter Mall'
+};
+export const footerTransInfo = {
+  linkFirst: 'frends link',
+  linkSecond: 'frends link',
+  linkThird: 'frends link',
+  linkForth: 'frends link',
+  companyName: 'Copyright @ 2010-2020 Shanghai TeMing Furniture Co., Ltd',
+  companyNumber: 'Hcpb06025014'
+};
