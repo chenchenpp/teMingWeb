99e6d094e8172806235b2678cdc63824b36179f2
chenpeng <2455915115@qq.com>
2020-07-15 00:24:50 +0800

    产品中心定位点数据添加

--- src/util/productData.js
+++ src/util/productData.js
@@ -48,16 +48,225 @@ export let productList = {
     [{
       isShow: false,
       left: '60%',
-      top: '82%',
+      top: '50%',
       isVideoFlag: true,
       src: 'dejia/video1.MP4'
+    }, {
+      isShow: false,
+      left: '90%',
+      top: '40%',
+      isVideoFlag: true,
+      src: 'dejia/video1.MP4'
+    }]
+  ],
+  missPointList: [
+    [{
+      isShow: false,
+      left: '27%',
+      top: '40%',
+      isVideoFlag: false,
+      src: 'miss/banner1-1.jpg',
+      title: '汉斯格雅龙头',
+      titleEnglish: 'Hansgeya tap',
+      des: '始于1901年，在任何时代都可彰显独具匠心与卓越售后的德国汉斯格雅系列洁具。',
+      desEnglish: 'Since 1901, hansgeya series sanitary ware of Germany can show its originality and excellent after-sale in any era.'
+    }, {
+      isShow: false,
+      left: '36%',
+      top: '50%',
+      isVideoFlag: false,
+      src: 'miss/banner1-2.jpg',
+      title: '平飞篮',
+      titleEnglish: 'Flying basket',
+      des: '封闭式篮框结构</br>弧面拐角、开放式端角设计</br>磁性防滑垫配置</br>静音防滑处理',
+      desEnglish: 'Closed basket frame structure </br> curved corner, open end angle design </br> magnetic anti-skid pad configuration </br> silent anti-skid treatment'
+    }, {
+      isShow: false,
+      left: '30%',
+      top: '32%',
+      isVideoFlag: false,
+      src: 'miss/banner1-3.jpg',
+      title: '超级连动拉篮',
+      titleEnglish: 'Super linked pull basket',
+      des: '智能型的排列</br>一览无遗的完整视觉效果</br>空间达到充分利用',
+      desEnglish: 'Intelligent arrangement </br> complete visual effect at a glance </br> space can be fully utilized'
+    }],
+    [{
+      isShow: false,
+      left: '86%',
+      top: '35%',
+      isVideoFlag: true,
+      src: 'dejia/video1.MP4'
+    }, {
+      isShow: false,
+      left: '37%',
+      top: '42%',
+      isVideoFlag: false,
+      src: 'miss/banner2-1.jpg',
+      title: '恒温暖灯',
+      titleEnglish: 'Constant temperature warm lamp',
+      des: '• 250瓦红外加热 &nbsp;&nbsp;&nbsp;• 约65°食物保温温度</br>• 保温亦保持口感&nbsp;&nbsp;&nbsp;• 启动简便</br>• 也可用于餐盘保温 &nbsp;&nbsp;&nbsp;• 节省解冻时间',
+      desEnglish: '• 250 watts infrared heating &nbsp;&nbsp;&nbsp;• about 65 ° food holding temperature </br>•Keep warm and keep taste&nbsp;&nbsp;&nbsp;• easy to start</br>• be used to keep dishes warm&nbsp;&nbsp;&nbsp;• save thawing time'
+    }, {
+      isShow: false,
+      left: '57%',
+      top: '56%',
+      isVideoFlag: false,
+      src: 'dejia/banner1.jpg',
+      title: '伊莱克斯电磁灶',
+      titleEnglish: 'Electrolux electromagnetic cooker',
+      des: '黑色镜面设计，进口微晶玻璃</br>锅具滑动温度调节，桥接功能灵活组合加热区域，14段功率调节精准温控，Booster功能瞬间功率提升，快速加热即可反应，智慧记忆自动续航',
+      desEnglish: 'Black mirror design, imported glass ceramics </br> the pot has sliding temperature adjustment, bridge function, flexible combination of heating area, 14 stage power adjustment, precise temperature control, booster function, instantaneous power improvement, quick heating reaction, intelligent memory automatic endurance'
+    }]
+  ],
+  dikesenPointList: [
+    [{
+      isShow: false,
+      left: '75%',
+      top: '58%',
+      isVideoFlag: false,
+      src: 'dikesen/banner1.jpg',
+      title: '高柜连动拉篮',
+      titleEnglish: 'High cabinet linked pull basket',
+      des: '全新的食品存储体验，只需轻轻拉开门，便给您带来无限的灵感。多篮筐的结构设计，合理分割柜内横纵向空间，在解决您细分收纳的同时，也能让您对柜内物品一目了然。',
+      desEnglish: 'The new food storage experience, just open the door gently, will bring you infinite inspiration. The structural design of multiple baskets can reasonably divide the horizontal and vertical space in the cabinet, which can not only solve the subdivision of storage, but also make the items in the cabinet clear at a glance'
+    }, {
+      isShow: false,
+      left: '30%',
+      top: '48%',
+      isVideoFlag: true,
+      src: 'dejia/video1.MP4'
+    }, {
+      isShow: false,
+      left: '58%',
+      top: '70%',
+      isVideoFlag: false,
+      src: 'dikesen/banner2.jpg',
+      title: '嵌入式轨道移动插座',
+      titleEnglish: 'Embedded rail mobile socket',
+      des: '随意拔插</br>任意增减</br>避免凿墙开孔</br>使用灵活便捷</br>独特安全设计',
+      desEnglish: 'Insert and insert at will</br>increase or decrease at will</br> avoid cutting holes in the wall</br> use flexible and convenient</br>unique safety design'
+    }, {
+      isShow: false,
+      left: '30%',
+      top: '64%',
+      isVideoFlag: true,
+      src: 'dejia/video1.MP4'
+    }]
+  ],
+  bierPointList: [
+    [{
+      isShow: false,
+      left: '10%',
+      top: '34%',
+      isVideoFlag: false,
+      src: 'bier/banner1-1.png',
+      title: '水晶宫',
+      titleEnglish: 'Crystal Palace',
+      des: '·设计风格时尚&nbsp;·篮筐高度“无级”调节</br>·“隐藏式”顶轨，增加有效空间&nbsp;·外观结构搭配细致</br>·轨道承重和阻尼效果优&nbsp;·开启轻松便捷 </br>·选配智能控制',
+      desEnglish: '·Fashionable design style &nbsp;·"Stepless" adjustment of basket height</br>·"Hidden" top rail to increase effective space &nbsp;·The appearance and structure are well matched </br>·Track load-bearing and damping effect is excellent &nbsp;·Easy to open </br>·Optional intelligent control',
+    }, {
+      isShow: false,
+      left: '36%',
+      top: '24%',
+      isVideoFlag: false,
+      src: 'bier/banner1-2.png',
+      title: '莫扎特墙挂',
+      titleEnglish: 'Mozart wall hanging',
+      des: '石墨黑与木纹的拼色，天然石材质感的表面处理，时尚且耐冲击、耐磨损；</br>自带LED照明系统；</br>各种置物架，可挂可放，随意组合；</br>充分利用桌面与吊柜间的墙面空间',
+      desEnglish: 'The color matching of ink black and wood grain, the surface treatment of natural stone texture, fashionable, impact resistant and wear-resistant; </br>with its own LED lighting system; </br> all kinds of shelves can be hung, placed and combined at will. </br> make full use of the wall space between the desktop and the cabinet'
     }],
     [{
       isShow: false,
       left: '60%',
-      top: '82%',
+      top: '30%',
+      isVideoFlag: false,
+      src: 'bier/banner2.jpg',
+      title: '人造石镜子',
+      titleEnglish: 'Artificial stone mirror',
+      des: '以铜为镜，可以正衣冠</br>以史为镜，可以知兴替</br>以人为镜，可以明得失</br>镜子是每个家庭不可或缺的物品。这款人造石镜子不仅只是一面可以照见人像的载体，它兼具了照明、时间和加热的功能，永不起雾的镜面以后将不再是梦想',
+      desEnglish: "With copper as a mirror, one can correct one's clothes and clothes.</br> history can be used as a mirror to know the rise and fall of people. </br>the mirror is an indispensable item for every family. This kind of artificial stone mirror is not only a carrier that can see human images, but also has the functions of lighting, time and heating. The mirror that never fogs will not be a dream in the future."
+    }],
+    [{
+      isShow: false,
+      left: '54%',
+      top: '32%',
       isVideoFlag: true,
       src: 'dejia/video1.MP4'
+    }]
+  ],
+  kelintePointList: [
+    [{
+      isShow: false,
+      left: '77%',
+      top: '22%',
+      isVideoFlag: false,
+      src: 'kelinte/banner1.jpg',
+      title: '擎天柱',
+      titleEnglish: 'skyreach pillar',
+      des: '时尚而简约的设计风格使功能与外观得到了完美的结合；</br>炭灰色表面与实木的组合更显尊贵气质，颜色搭配美观、时尚；',
+      desEnglish: 'The fashionable and simple design style makes the function and appearance perfectly combined.</br> the combination of charcoal grey surface and solid wood shows more noble temperament, and the color matching is beautiful and fashionable;'
+    }],
+    [{
+      isShow: false,
+      left: '55%',
+      top: '41%',
+      isVideoFlag: false,
+      src: 'kelinte/banner2.jpg',
+      title: '开架45°拼接工艺',
+      titleEnglish: '45 ° open frame splicing process',
+      des: '待补充',
+      desEnglish: ''
+    }],
+    [{
+      isShow: false,
+      left: '12%',
+      top: '34%',
+      isVideoFlag: false,
+      src: 'kelinte/banner3.jpg',
+      title: '连门宝',
+      titleEnglish: 'Linked door baby',
+      des: '厨柜门板不再仅仅是一扇门。有了连门宝，只需开门这一简单的动作，即可轻松取放物品。托盘高度可调，灵活至极；挂框尺寸可以适配于厨柜的高柜、地柜和吊柜等区域。',
+      desEnglish: 'Kitchen cabinet door panel is no longer just a door. With lianmenbao, you just need to open the door this simple action, you can easily take and put items. The height of the tray is adjustable and flexible; the size of the hanging frame can be adapted to the high cabinet, floor cabinet and wall cabinet of the kitchen cabinet.'
+    }, {
+      isShow: false,
+      left: '50%',
+      top: '34%',
+      isVideoFlag: true,
+      src: 'dejia/video1.MP4'
+    }]
+  ],
+  botiqieliPointList: [
+    [{
+      isShow: false,
+      left: '17%',
+      top: '77%',
+      isVideoFlag: false,
+      src: 'dejia/banner1.jpg',
+      title: '大力士',
+      titleEnglish: 'Hercules',
+      des: '木质套柜全拉出解决方案，小身材大容量。',
+      desEnglish: 'Wooden cabinet full pull-out solution, small size and large capacity.'
+    },{
+      isShow: false,
+      left: '17%',
+      top: '77%',
+      isVideoFlag: false,
+      src: 'dejia/banner1.jpg',
+      title: '衣柜鞋架',
+      titleEnglish: 'Wardrobe shoe rack',
+      des: '摆放有序，</br>清晰明朗',
+      desEnglish: 'Orderly placement,</br> clear and clear'
+    },{
+      isShow: false,
+      left: '17%',
+      top: '77%',
+      isVideoFlag: false,
+      src: 'dejia/banner1.jpg',
+      title: '270°全景窄边玻璃铝框门',
+      titleEnglish: '270 ° panoramic narrow edge glass aluminum frame door',
+      des: '270°全开玻璃铝框门</br>给予收纳取物最大的便利',
+      desEnglish: '270 ° full open glass aluminum frame door</br> provides the greatest convenience for storage and retrieval'
     }]
   ]
 };
